143a49550b571747fc8137a877cba764a4fd860c
aptheparker <apapap1119@g.skku.edu>
2024-02-11 18:03:02 +0900

    feat: first init with metadata settings

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'aptheparker',
+  description: 'I post about programming and web development.',
+};
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the home page</p>
+    </div>
+  );
+}
