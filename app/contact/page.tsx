61d2d82c887843b7d2d55301cbce7a034611cb15
aptheparker <apapap1119@g.skku.edu>
2024-02-18 20:32:53 +0900

    feat: contact page added to route

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactForm from "@/components/contact-page/contact-form";
+
+export const metadata = {
+  title: "Contact Me | aptheparker",
+  description: "Send me your messages.",
+};
+
+export default function ContactPage() {
+  return <ContactForm/>;
+}
